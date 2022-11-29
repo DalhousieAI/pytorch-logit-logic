55c27d8a614a67d7bb4c9e415cca6be4d8c130b9
Scott Lowe <scott.code.lowe@gmail.com>
2022-11-29 12:35:40 -0600

    REL: Release Version 1.0.0

--- pytorch_logit_logic/__meta__.py
+++ pytorch_logit_logic/__meta__.py
@@ -1,6 +1,6 @@
 name = "pytorch-logit-logic"
 path = name.lower().replace("-", "_").replace(" ", "_")
-version = "0.1.dev0"
+version = "1.0.0"
 author = "Scott C. Lowe"
 author_email = "scottclowe@gmail.com"
 description = "Logit-space logical activation functions for pytorch"
